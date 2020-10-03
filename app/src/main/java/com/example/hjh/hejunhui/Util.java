716c03b9b2efca8f8f6754e99a0e2d25356588e9
nyj1999 <1528464853@qq.com>
2020-10-03 23:47:38 +0800

    第一次作业

---
+++ app/src/main/java/com/example/hjh/hejunhui/Util.java
@@ -0,0 +1,13 @@
+package com.example.work01;
+import android.content.Context;
+
+public class Util {
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+    public static int p2xdip(Context context, float pxValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue * scale + 0.5f);
+    }
+}
